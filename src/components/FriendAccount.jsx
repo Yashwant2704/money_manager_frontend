100bb8a44c5ef320e267404d4eb1c7f58b7d82d9
Yashwant2704 <155355507+Yashwant2704@users.noreply.github.com>
2025-03-24 10:27:08 +0530

    Remove ₹ from table header

--- src/components/FriendAccount.jsx
+++ src/components/FriendAccount.jsx
@@ -129,7 +129,7 @@ function FriendAccount({ friend, refresh }) {
               <thead>
                 <tr>
                   <th>Date</th>
-                  <th>Amount (₹)</th>
+                  <th>Amount</th>
                   <th>Note</th>
                 </tr>
               </thead>
