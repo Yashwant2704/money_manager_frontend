014554660b0560613a2fd962043052e7ecccc90a
Yashwant Nagarkar <yashwantnagarkar04@gmail.com>
2025-09-20 19:24:37 +0530

    Fix title setting on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,11 +2,16 @@ import { useState } from "react";
 import AddFriendForm from "../components/AddFriendForm";
 import FriendList from "../components/FriendList";
 import "./Home.css";
+import { useEffect } from "react";
 
 function Home() {
   const [refreshTrigger, setRefreshTrigger] = useState(0);
 
   const triggerRefresh = () => setRefreshTrigger((prev) => prev + 1);
+
+  useEffect(()=>{
+    document.title="YMoneyManager"
+  })
 
   return (
     <div className="home">
